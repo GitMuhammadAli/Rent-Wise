b2fe1db12c3dfc7e688b27c83099ba15e5fd2a44
malingxoft <malingxoft@gmail.com>
2024-10-24 21:29:55 +0500

    update-dashboard-user

--- server/routes/dashboard/dashboardRoute.js
+++ server/routes/dashboard/dashboardRoute.js
@@ -3,4 +3,7 @@ const UserDashboard = require("../../controller/dashboard/dashboard");
 
 
 router.get("/getUserDashboard", UserDashboard.GetUser);
+
+
+router.put("/updateUserDashboard", UserDashboard.updateUserDashboard);
 module.exports = router
