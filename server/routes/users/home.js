fc12f05d32d46153c3bc24b02c763870ba8c1d09
malingxoft <malingxoft@gmail.com>
2024-11-16 22:53:17 +0500

    improvements

--- server/routes/users/home.js
+++ server/routes/users/home.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const { AuthorizeUser, FindUser } = require("../../middleware/auth");
 const home = require("../../controller/user/home");
 
-router.get("/auth/user/home", AuthorizeUser("user" , "admin"), home.userHome);
+const { asyncHandler} = require('../../middleware/asyncWrapper');
+
+router.get("/auth/user/home", AuthorizeUser("user" , "admin"), asyncHandler(home.userHome));
 
 
 module.exports = router;
