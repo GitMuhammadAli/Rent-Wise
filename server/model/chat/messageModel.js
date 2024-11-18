651693c445ed0b6d826ecb7e94261017472ee0c5
malingxoft <malingxoft@gmail.com>
2024-11-18 22:43:14 +0500

    chat-models

---
+++ server/model/chat/messageModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const MessageSchema = new mongoose.Schema({
+        sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+        receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+        message: { type: String, required: true },
+        createdAt: { type: Date, default: Date.now },
+        updatedAt: { type: Date, default: Date.now },
+    });
+    
+    const Message = mongoose.model("Message", MessageSchema);
+    module.exports = Message
