b5a3ba39a8a7055b95531f39fa372e3192be44c0
malingxoft <malingxoft@gmail.com>
2025-01-13 23:01:25 +0500

    comit

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,9 @@
 const Users = require("../model/user/userModel");
 const jsonwebtoken = require("jsonwebtoken");
-const {BOOLEAN} = require("../utils/Roles");
+const { BOOLEAN } = require("../utils/Roles");
+const AppError = require("../utils/AppError");
+const { ROLES, } = require("../utils/Roles");
+const { STATUS } = require("../messages/status")
 
 
 function clearCookies(req, res) {
@@ -10,10 +13,10 @@ function clearCookies(req, res) {
 exports.AuthorizeUser = (RequiredRole) => {
   return async (req, res, next) => {
     const token = req.cookies.jwt;
-   console.log("JWT Cookie:", token); // Debugging
+    console.log("JWT Cookie:", token); // Debugging
 
     if (!token) {
-      return res.status(401).json({ message: "No token provided" });
+      return res.status(STATUS.UNAUTHORIZED).json({ message: "No token provided" });
     }
 
     try {
@@ -21,20 +24,20 @@ exports.AuthorizeUser = (RequiredRole) => {
       const user = await Users.findById(decodedToken._id);
 
       if (!user) {
-        clearCookies(req, res);  
-        return res.status(401).json({ message: "User not found" });
+        clearCookies(req, res);
+        return res.status(STATUS.UNAUTHORIZED).json({ message: "User not found" });
       }
 
-      if (user.role === RequiredRole || (RequiredRole === "user" && user.role === "admin")) {
+      if (user.role === RequiredRole || (RequiredRole === ROLES.USER && user.role === ROLES.ADMIN)) {
         req.user = user;
         return next();
       } else {
-        clearCookies(req, res);  
-        return res.status(403).json({ message: "Forbidden" });
+        clearCookies(req, res);
+        return next(new AppError(BOOLEAN.FALSE, "Unauthorized - Invalid Role", STATUS.UNAUTHORIZED));
       }
     } catch (error) {
-      clearCookies(req, res); 
-      return res.status(401).json({ message: "Invalid token" });
+      clearCookies(req, res);
+      next(error);
     }
   };
 };
@@ -66,7 +69,6 @@ exports.FindUser = async (req, res, next) => {
 
     next();
   } catch (error) {
-    console.error("Error verifying OTP:", error);
-    return res.status(401).json({ message: "Internal server error" });
+    next(error);
   }
 };
--- server/middleware/checkAuth.js
+++ server/middleware/checkAuth.js
@@ -21,12 +21,11 @@ exports.protectRoute = async (req, res, next) => {
     }
 
     // Attaching the authenticated user object to the request
-    // This makes the user data available to all subsequent middleware and route handlers
     req.user = user;
 
     next();
   } catch (error) {
     console.log("Error in protectRoute middleware: ", error.message);
-    res.status(500).json({ message: "Internal server error" });
+  next(error);
   }
 };
