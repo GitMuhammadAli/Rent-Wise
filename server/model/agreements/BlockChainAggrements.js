ba6459861e1d217997ac2e7e1e883a97bb04ea76
malingxoft <malingxoft@gmail.com>
2025-01-16 21:23:45 +0500

    com

---
+++ server/model/agreements/BlockChainAggrements.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+
+
+const BlockChainAggrementSchema = new mongoose.Schema({
+    AggrementId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Aggrement",
+        required: true,
+    },
+    status:{
+        enum:["pending", "accepted", "rejected" , "active"],
+        type:String,
+        required:true,
+        default:"pending"
+    }
+});
+
+
+
+const BlockChainAggrement = mongoose.model("BlockChainAggrement", BlockChainAggrementSchema);
+
+module.exports = BlockChainAggrement;
