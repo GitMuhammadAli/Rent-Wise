4c5c9d470d7c00a214a9f4f562ed4f8791a27283
malingxoft <malingxoft@gmail.com>
2025-01-21 23:32:10 +0500

    com

--- server/controller/homePage/listingsCategories.js
+++ server/controller/homePage/listingsCategories.js
@@ -3,21 +3,19 @@ const listings = require("../../model/listings/RentalItemModel")
 const AggrementDetails = require("../../model/agreements/AggrementDetails");
 const RentalItem = require("../../model/listings/RentalItemModel");
 const { ERROR_MESSAGE } = require("../../messages/error");
-const { RESPONCE_MESSAGE, AGGREEMENT, CONVERSATION ,LISTINGS } = require("../../messages/response");
+const { RESPONCE_MESSAGE, AGGREEMENT, CONVERSATION, LISTINGS } = require("../../messages/response");
 const { STATUS } = require("../../messages/status");
 const AppError = require("../../utils/AppError");
 const { ROLES, BOOLEAN } = require("../../utils/Roles");
 const QRCode = require('qrcode')
 const { io } = require("../../utils/socket");
-const Messsage = require("../../model/chat/MesssageModel");
-const Conversation = require("../../model/chat/ConversationModel");
 
 
 
 exports.getAllCarsListings = async (req, res, next) => {
     try {
         const CarListings = await listings.find({
-            category:"car"
+            category: "car"
         })
 
         res.status(STATUS.SUCCESS).json({
@@ -34,7 +32,7 @@ exports.getAllCarsListings = async (req, res, next) => {
 exports.getAllHouseListings = async (req, res, next) => {
     try {
         const HouseListings = await listings.find({
-            category:"house"
+            category: "house"
         })
 
         res.status(STATUS.SUCCESS).json({
@@ -53,7 +51,7 @@ exports.getAllApartmentsListings = async (req, res, next) => {
     try {
 
         const ApartmentListings = await listings.find({
-            category:"apartment"
+            category: "apartment"
         })
 
         res.status(STATUS.SUCCESS).json({
