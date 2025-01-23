e894632a9344746fcae365721d50bcfb718bb437
malingxoft <malingxoft@gmail.com>
2025-01-23 16:13:43 +0500

    push

--- server/controller/dashboard/renter.js
+++ server/controller/dashboard/renter.js
@@ -17,7 +17,7 @@ const Conversation = require("../../model/chat/ConversationModel");
 exports.fetchRenterAggreements = async (req, res, next)=>{
     try {
         const renter = req.user._id;
-        const aggreements = await Aggrement.find({renterId:renter}).populate("agreementDetailsId")
+        const aggreements = await Aggrement.find({renterId:renter}).populate("agreementDetailsId").populate("ownerId").populate("listingId")
         if(aggreements){
             res.status(STATUS.SUCCESS).json({
                 status: BOOLEAN.TRUE,
